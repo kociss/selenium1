c6e131ca6b622f4ba554651ca19f2286b00bf7fa
Witold <kociss@wp.pl>
2018-08-12 12:03:33 +0200

    Zmiana IP

--- src/test/java/pl/lait/selenium1/Init.java
+++ src/test/java/pl/lait/selenium1/Init.java
@@ -1,10 +1,13 @@
 package pl.lait.selenium1;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Init {
 
@@ -12,14 +15,24 @@ public class Init {
 
 	public static WebDriver getDriver() {
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\kociss\\Downloads\\geckodriver.exe");
-
-		DesiredCapabilities cap = DesiredCapabilities.firefox();
+        URL url = null;
+        
+        try {
+			url = new URL("https://192.168.0.112:4444/wd/hub");           //konstruktor URL
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}                                                         
+        
+		
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
 		if (driver == null) {
 
-			driver = new FirefoxDriver(cap);
+			//driver = new FirefoxDriver(cap);
+			driver = new RemoteWebDriver(url, cap);
 			driver.get("http://newtours.demoaut.com");
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
+			//driver.manage().window().maximize();
 			return driver;
 		} else {
 			return driver;
